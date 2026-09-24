3d8ebcf0adad7e841048bcccf54a112428efa48f
Priscilla Cahino <priscillacahinoo@gmail.com>
2026-09-24 12:10:47 -0300

    Atualiza identidade para Amigos da Vênus V8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tpb-v7-20260924';
+const CACHE='amigos-venus-v8-20260924';
 const ASSETS=['./','index.html','styles.css','app.js','manifest.json','data/imoveis.json','data/meta.json','icons/icon-192.png','icons/icon-512.png','icons/icon-maskable-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
